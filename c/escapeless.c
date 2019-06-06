1bddcecf23e98eb0955eeb8dd2113106440c27e1
Ivan Kosarev <ivan@kosarev.info>
2019-06-06 17:31:58 +0300

    Get rid of the 'lib' suffix in file names.

--- c/escapeless.c
+++ c/escapeless.c
@@ -8,142 +8,92 @@
     Published under the MIT license.
 */
 
-#include <errno.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 
-#include "libescapeless.h"
+#include "escapeless.h"
 
-static void verror(const char *format, va_list args) {
-    fprintf(stderr, "escapeless: ");
-    vfprintf(stderr, format, args);
-    fprintf(stderr, "\n");
-    exit(EXIT_FAILURE);
-}
+void escapeless_encode(unsigned char *takeouts_map,
+                       const unsigned char *takeouts, unsigned takeouts_size,
+                       unsigned char *block, unsigned block_size) {
+    /* Initially map[c] != c for any character c. */
+    unsigned char map[0x100] = { 0x01 };
+    unsigned i, k, c;
 
-static void error(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    verror(format, args);
-    va_end(args);
-}
+    assert(takeouts_size < 0x100 - 1);
+    assert(block_size <= 0x100 - 1 - takeouts_size);
 
-static int read_byte(void) {
-    int c = getchar();
-    if(c == EOF && ferror(stdin))
-        error("cannot read input data: %s", strerror(errno));
-    return c;
-}
+    /* Start with takeouts mapped to non-themselves, so
+       takeouts_map[k] != takeouts[k] for them. */
+    for(k = 0; k < takeouts_size; k++)
+        takeouts_map[k] = takeouts[k] ^ 1;
 
-static void write_byte(int c) {
-    if(putchar(c) == EOF)
-        error("cannot write output data: %s", strerror(errno));
-}
+    /* Mark characters used in the block with map[c] == c. */
+    for(i = 0; i < block_size; i++)
+        map[ block[i] ] = block[i];
 
-static void encode(const unsigned char *takeouts, unsigned takeouts_size) {
-    unsigned char block[0x100], takeouts_map[0x100];
-    unsigned max_block_size, block_size;
-    unsigned i;
-    int c;
-
-    /* TODO: Validate takeouts size. */
-
-    max_block_size = 0x100 - 1 - takeouts_size;
-
-    for(;;) {
-        /* Read subsequent block of data. */
-        block_size = 0;
-        while(block_size < max_block_size && (c = read_byte()) != EOF)
-            block[block_size++] = (unsigned char) c;
-
-        if(block_size == 0)
-            break;
-
-        /* Encode that block. */
-        escapeless_encode(takeouts_map, takeouts, takeouts_size,
-                          block, block_size);
-
-        /* Write the substitutes for the takeout bytes. */
-        for(i = 0; i < takeouts_size; i++)
-            write_byte(takeouts_map[i]);
-
-        /* Write the block with replaced takeout bytes. */
-        for(i = 0; i < block_size; i++)
-            write_byte(block[i]);
+    /* Similarly, mark unused takeouts with map[c] == c and map
+       them to themselves, so takeouts_map[k] == takeouts[k]. */
+    for(k = 0; k < takeouts_size; k++) {
+        c = takeouts[k];
+        if(map[c] != c) {
+            map[c] = (unsigned char) c;
+            takeouts_map[k] = (unsigned char) c;
+        }
     }
-}
 
-static void decode(const unsigned char *takeouts, unsigned takeouts_size) {
-    unsigned char block[0x100], takeouts_map[0x100];
-    unsigned max_block_size, block_size;
-    unsigned i;
-    int c;
-
-    /* TODO: Validate takeouts size. */
-
-    max_block_size = 0x100 - 1 - takeouts_size;
-
+    /* Map used takeouts to unmarked values. */
+    c = 0;
+    k = 0;
     for(;;) {
-        /* Read the substitutes for the takeout bytes. */
-        for(i = 0; i < takeouts_size; i++) {
-            if((c = read_byte()) == EOF)
-                break;
-            takeouts_map[i] = (unsigned char) c;
+        assert(c < 0x100);
+        if(map[c] == c) {
+            c++;
+            continue;
         }
 
-        /* See of we reached the end of file. */
-        if(i == 0)
-            break;
+        if(k < takeouts_size) {
+            if(takeouts_map[k] != takeouts[k])
+                takeouts_map[k] = (unsigned char) c++;
+            k++;
+            continue;
+        }
 
-        if(i < takeouts_size)
-            error("unexpected end of file");
-
-        /* Read the payload part of the block. */
-        block_size = 0;
-        while(block_size < max_block_size && (c = getchar()) != EOF)
-            block[block_size++] = (unsigned char) c;
-
-        /* Empty block should have never been encoded. */
-        if(block_size == 0)
-            error("empty block in input");
-
-        /* Decode the block. */
-        escapeless_decode(takeouts_map, takeouts, takeouts_size,
-                          block, block_size);
-
-        /* Write decoded block. */
-        for(i = 0; i < block_size; i++)
-            write_byte(block[i]);
-    }
-}
-
-static void usage(void) {
-    fprintf(stderr, "usage: escapeless {encode|decode} <takeouts>\n");
-    exit(EXIT_FAILURE);
-}
-
-int main(int argc, char *argv[]) {
-    const char *command, *takeouts;
-    size_t takeouts_size;
-
-    if(argc != 3)
-        usage();
-
-    takeouts = argv[2];
-    takeouts_size = strlen(takeouts);
-    if(takeouts_size >= 0x100 - 1)
-        error("too many takeout bytes");
-
-    command = argv[1];
-    if(strcmp(command, "encode") == 0) {
-        encode((const unsigned char*) takeouts, (unsigned) takeouts_size);
-    } else if(strcmp(command, "decode") == 0) {
-        decode((const unsigned char*) takeouts, (unsigned) takeouts_size);
-    } else {
-        error("unknown command: %s", command);
+        break;
     }
 
-    return EXIT_SUCCESS;
+    /* Map all unused takeouts to a (or the) remaining unmarked
+       value. */
+    for(k = 0; k < takeouts_size; k++) {
+        if(takeouts_map[k] == takeouts[k])
+            takeouts_map[k] = (unsigned char) c;
+    }
+
+    /* Propagate the takeouts map to the byte values map. */
+    for(k = 0; k < takeouts_size; k++)
+        map[ takeouts[k] ] = takeouts_map[k];
+
+    /* Replace used takeouts. */
+    for(i = 0; i < block_size; i++)
+        block[i] = map[ block[i] ];
+}
+
+void escapeless_decode(const unsigned char *takeouts_map,
+                       const unsigned char *takeouts, unsigned takeouts_size,
+                       unsigned char *block, unsigned block_size) {
+    unsigned char map[0x100];
+    unsigned i, k;
+
+    assert(takeouts_size < 0x100 - 1);
+    assert(block_size <= 0x100 - 1 - takeouts_size);
+
+    for(i = 0; i < 0x100; i++)
+        map[i] = (unsigned char) i;
+
+    /* Map takeouts to their original values. */
+    for(k = 0; k < takeouts_size; k++)
+        map[ takeouts_map[k] ] = takeouts[k];
+
+    /* Restore takeouts in the block. */
+    for(i = 0; i < block_size; i++)
+        block[i] = map[ block[i] ];
 }
---
+++ c/escapeless_cli.c
@@ -0,0 +1,149 @@
+
+/*  Efficient binary encoding for large alphabets.
+    https://github.com/kosarev/escapeless
+
+    Copyright (C) 2019 Ivan Kosarev.
+    ivan@kosarev.info
+
+    Published under the MIT license.
+*/
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "escapeless.h"
+
+static void verror(const char *format, va_list args) {
+    fprintf(stderr, "escapeless: ");
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+static void error(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    verror(format, args);
+    va_end(args);
+}
+
+static int read_byte(void) {
+    int c = getchar();
+    if(c == EOF && ferror(stdin))
+        error("cannot read input data: %s", strerror(errno));
+    return c;
+}
+
+static void write_byte(int c) {
+    if(putchar(c) == EOF)
+        error("cannot write output data: %s", strerror(errno));
+}
+
+static void encode(const unsigned char *takeouts, unsigned takeouts_size) {
+    unsigned char block[0x100], takeouts_map[0x100];
+    unsigned max_block_size, block_size;
+    unsigned i;
+    int c;
+
+    /* TODO: Validate takeouts size. */
+
+    max_block_size = 0x100 - 1 - takeouts_size;
+
+    for(;;) {
+        /* Read subsequent block of data. */
+        block_size = 0;
+        while(block_size < max_block_size && (c = read_byte()) != EOF)
+            block[block_size++] = (unsigned char) c;
+
+        if(block_size == 0)
+            break;
+
+        /* Encode that block. */
+        escapeless_encode(takeouts_map, takeouts, takeouts_size,
+                          block, block_size);
+
+        /* Write the substitutes for the takeout bytes. */
+        for(i = 0; i < takeouts_size; i++)
+            write_byte(takeouts_map[i]);
+
+        /* Write the block with replaced takeout bytes. */
+        for(i = 0; i < block_size; i++)
+            write_byte(block[i]);
+    }
+}
+
+static void decode(const unsigned char *takeouts, unsigned takeouts_size) {
+    unsigned char block[0x100], takeouts_map[0x100];
+    unsigned max_block_size, block_size;
+    unsigned i;
+    int c;
+
+    /* TODO: Validate takeouts size. */
+
+    max_block_size = 0x100 - 1 - takeouts_size;
+
+    for(;;) {
+        /* Read the substitutes for the takeout bytes. */
+        for(i = 0; i < takeouts_size; i++) {
+            if((c = read_byte()) == EOF)
+                break;
+            takeouts_map[i] = (unsigned char) c;
+        }
+
+        /* See of we reached the end of file. */
+        if(i == 0)
+            break;
+
+        if(i < takeouts_size)
+            error("unexpected end of file");
+
+        /* Read the payload part of the block. */
+        block_size = 0;
+        while(block_size < max_block_size && (c = getchar()) != EOF)
+            block[block_size++] = (unsigned char) c;
+
+        /* Empty block should have never been encoded. */
+        if(block_size == 0)
+            error("empty block in input");
+
+        /* Decode the block. */
+        escapeless_decode(takeouts_map, takeouts, takeouts_size,
+                          block, block_size);
+
+        /* Write decoded block. */
+        for(i = 0; i < block_size; i++)
+            write_byte(block[i]);
+    }
+}
+
+static void usage(void) {
+    fprintf(stderr, "usage: escapeless {encode|decode} <takeouts>\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    const char *command, *takeouts;
+    size_t takeouts_size;
+
+    if(argc != 3)
+        usage();
+
+    takeouts = argv[2];
+    takeouts_size = strlen(takeouts);
+    if(takeouts_size >= 0x100 - 1)
+        error("too many takeout bytes");
+
+    command = argv[1];
+    if(strcmp(command, "encode") == 0) {
+        encode((const unsigned char*) takeouts, (unsigned) takeouts_size);
+    } else if(strcmp(command, "decode") == 0) {
+        decode((const unsigned char*) takeouts, (unsigned) takeouts_size);
+    } else {
+        error("unknown command: %s", command);
+    }
+
+    return EXIT_SUCCESS;
+}
